d35c4134d80cca8ac67f2b71ff3d36371de44255
demi0504 <ripley.demi@gmail.com>
2020-07-24 13:30:03 -0500

    added express static to server file

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ var express = require("express");
 var app = express();
 
 // Sets an initial port
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + "/public"));
 
 //routes for the server to use to respond to user
 require("./routes/apiRoutes")(app);
