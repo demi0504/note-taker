820b1b12be81eb8b533a969b22862f6d02a2ef3c
demi0504 <ripley.demi@gmail.com>
2020-07-24 12:09:25 -0500

    delete added to api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,57 +1,38 @@
 const db = require("../db/db.json");
 const fs = require("fs");
-
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
+//routing
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-
-  app.get("/api/tables", function(req, res) {
-    res.json(tableData);
+  //API GET request
+  app.get("/api/notes", function(req, res) {
+    res.json(db);
   });
 
-  app.get("/api/waitlist", function(req, res) {
-    res.json(waitListData);
+  //API POST request
+  app.post("/api/notes", function(req, res) {
+    //receive new note to save on reqest body, addd it to db.json
+    db.push(req.body);
+    //add unique id to each note
+    db.forEach((obj, i) => {
+        obj.id = i + 1;
+    })
+    //return new note to client
+    fs.writeFile("./db/db.json", JSON.stringify(db), function (){
+        res.json(db);
+    })
   });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
-
-  app.post("/api/tables", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
-  });
-
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  app.post("/api/clear", function(req, res) {
-    // Empty out the arrays of data
-    tableData.length = 0;
-    waitListData.length = 0;
-
-    res.json({ ok: true });
+  //API DELETE request
+  app.delete("/api/notes/:id", function(req, res) {
+    var id = req.params.id;
+    //splice deletes selected note from the db array
+    db.splice(id - 1, 1);
+    //reassign id for each note object
+    db.forEach((obj, i) => {
+        obj.id = i + 1;
+    });
+    //return remaining notes to client
+    fs.writeFile("./db/db.json", JSON.stringify(db), function (){
+        res.json(db);
+    });
   });
 };
