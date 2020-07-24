f82ead4a3dd7008b81d66d3149f4bae67bd4788c
demi0504 <ripley.demi@gmail.com>
2020-07-24 11:46:40 -0500

    require express, api, html routes added to server

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+
+var app = express();
+
+// Sets an initial port
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//routes for the server to use to respond to user
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
